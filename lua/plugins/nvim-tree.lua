c548c89ec1f98c44085d52e6529133dfa163c8a8
visualpivert <visualpivert@gmail.com>
2025-03-28 00:12:01 +0300

    feat: ignore somme folder in nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,6 +1,10 @@
 require("nvim-tree").setup({
 	view = {
 		side = "right"
+	},
+	filters = {
+		-- Ignorer les dossiers spécifiques
+		custom = {".git", "node_modules", "dist"}  -- Liste des dossiers à ignorer
 	}
 })
 
